644891bf00f4449b6959b7f04024ad6571b03fff
jwleefine <83448143+jwleefine@users.noreply.github.com>
2024-04-24 15:15:11 +0900

    Update iTransformer.sh

--- scripts/multivariate_forecasting/fx/iTransformer.sh
+++ scripts/multivariate_forecasting/fx/iTransformer.sh
@@ -1,111 +1,110 @@
-export CUDA_VISIBLE_DEVICES=2
-
+export CUDA_VISIBLE_DEVICES=2 
 model_name=iTransformer
 
 python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/fx/ \
-  --data_path fx.csv \
-  --model_id traffic_96_96 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 20 \
-  --label_len 5
-  --pred_len 5 \
-  --e_layers 4 \
-  --enc_in 25 \
-  --dec_in 25 \
-  --c_out 25 \
-  --des 'Exp' \
-  --d_model 512\
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.001 \
-  --itr 1 \
-  --target_root_path='./dataset/fx/' \
-  --target_data_path='fx.csv' \
-  --use_gpu True \
-  --gpu 0 \
-  --freq = 'd'
+--is_training 1 \
+--root_path ./dataset/fx/ \
+--data_path fx.csv \
+--model_id traffic_96_96 \
+--model $model_name \
+--data custom \
+--features M \
+--seq_len 20 \
+--label_len 5 \
+--pred_len 5 \
+--e_layers 4 \
+--enc_in 25 \
+--dec_in 25 \
+--c_out 25 \
+--des 'Exp' \
+--d_model 512 \
+--d_ff 512 \
+--batch_size 16 \
+--learning_rate 0.001 \
+--itr 1 \
+--target_root_path='./dataset/fx/' \
+--target_data_path='fx.csv' \
+--use_gpu True \
+--gpu 0 \
+--freq 'd'
 
 python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/fx/ \
-  --data_path fx.csv \
-  --model_id traffic_96_192 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 20 \
-  --label_len 5
-  --pred_len 10 \
-  --e_layers 4 \
-  --enc_in 25 \
-  --dec_in 25 \
-  --c_out 25 \
-  --des 'Exp' \
-  --d_model 512 \
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.001 \
-  --itr 1 \
-  --target_root_path='./dataset/fx/' \
-  --target_data_path='fx.csv'\
-  --use_gpu True \
-  --gpu 0 \
-  --freq = 'd'
+--is_training 1 \
+--root_path ./dataset/fx/ \
+--data_path fx.csv \
+--model_id traffic_96_192 \
+--model $model_name \
+--data custom \
+--features M \
+--seq_len 20 \
+--label_len 5 \
+--pred_len 10 \
+--e_layers 4 \
+--enc_in 25 \
+--dec_in 25 \
+--c_out 25 \
+--des 'Exp' \
+--d_model 512 \
+--d_ff 512 \
+--batch_size 16 \
+--learning_rate 0.001 \
+--itr 1 \
+--target_root_path='./dataset/fx/' \
+--target_data_path='fx.csv' \
+--use_gpu True \
+--gpu 0 \
+--freq 'd'
 
 python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/fx/ \
-  --data_path fx.csv \
-  --model_id traffic_96_336 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 20 \
-  --label_len 5
-  --pred_len 15 \
-  --e_layers 4 \
-  --enc_in 25 \
-  --dec_in 25 \
-  --c_out 25 \
-  --des 'Exp' \
-  --d_model 512\
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.001 \
-  --itr 1 \
-  --target_root_path='./dataset/fx/' \
-  --target_data_path='fx.csv'\
-  --use_gpu True \
-  --gpu 0 \
-  --freq = 'd'
+--is_training 1 \
+--root_path ./dataset/fx/ \
+--data_path fx.csv \
+--model_id traffic_96_336 \
+--model $model_name \
+--data custom \
+--features M \
+--seq_len 20 \
+--label_len 5 \
+--pred_len 15 \
+--e_layers 4 \
+--enc_in 25 \
+--dec_in 25 \
+--c_out 25 \
+--des 'Exp' \
+--d_model 512 \
+--d_ff 512 \
+--batch_size 16 \
+--learning_rate 0.001 \
+--itr 1 \
+--target_root_path='./dataset/fx/' \
+--target_data_path='fx.csv' \
+--use_gpu True \
+--gpu 0 \
+--freq 'd'
 
 python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/fx/ \
-  --data_path fx.csv \
-  --model_id traffic_96_720 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 20 \
-  --label_len 5
-  --pred_len 20 \
-  --e_layers 4 \
-  --enc_in 25 \
-  --dec_in 25 \
-  --c_out 25 \
-  --des 'Exp' \
-  --d_model 512 \
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.001\
-  --itr 1 \
-  --target_root_path='./dataset/fx/' \
-  --target_data_path='fx.csv'\
-  --use_gpu True \
-  --gpu 0 \
-  --freq = 'd'
+--is_training 1 \
+--root_path ./dataset/fx/ \
+--data_path fx.csv \
+--model_id traffic_96_720 \
+--model $model_name \
+--data custom \
+--features M \
+--seq_len 20 \
+--label_len 5 \
+--pred_len 20 \
+--e_layers 4 \
+--enc_in 25 \
+--dec_in 25 \
+--c_out 25 \
+--des 'Exp' \
+--d_model 512 \
+--d_ff 512 \
+--batch_size 16 \
+--learning_rate 0.001 \
+--itr 1 \
+--target_root_path='./dataset/fx/' \
+--target_data_path='fx.csv' \
+--use_gpu True \
+--gpu 0 \
+--freq 'd'
